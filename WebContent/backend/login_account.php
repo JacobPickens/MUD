af4885a6896bb03ad72638c017deaad8b817639a
Jacob Pickens <pickuptruck2000@gmail.com>
2017-11-12 17:09:16 -0600

    I did a lot of stuff.

---
+++ WebContent/backend/login_account.php
@@ -0,0 +1,46 @@
+<?php 
+
+$host = "localhost";
+$database = "id3595091_accounts";
+$user = "id3595091_accounts";
+$password = "12hotnj";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if(!$conn) {
+	die("Connection failed: " + mysqli_connect_error());
+}
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+
+$query = "SELECT * FROM accounts WHERE username=\"$username\"";
+$results = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($results) > 0) {
+	$id = 0;
+	while($row = mysqli_fetch_assoc($results)) {
+		$id = $row['id'];
+	}
+	
+	// Login
+	$query = "SELECT * FROM accounts WHERE id=$id";
+	$results = mysqli_query($conn, $query);
+	
+	$returnedPassword = "";
+	while($row = mysqli_fetch_assoc($results)) {
+		$returnedPassword = $row['password'];
+	}
+	
+	if($password == $returnedPassword) {
+		echo $id;
+	} else {
+		echo 'failed'; // Password incorrect
+	}
+} else {
+	echo 'failed'; // User doesn't exist
+}
+
+mysqli_close($conn);
+
+?>
---
+++ WebContent/backend/register_account.php
@@ -0,0 +1,34 @@
+<?php 
+
+$host = "localhost";
+$database = "id3595091_accounts";
+$user = "id3595091_accounts";
+$password = "12hotnj";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if(!$conn) {
+	die("Connection failed: " + mysqli_connect_error());
+}
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+
+$query = "SELECT * FROM accounts WHERE username=\"$username\"";
+$results = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($results) > 0) {
+	echo 'exists';
+} else {
+	$query = "INSERT INTO accounts(username, password) VALUES(\"$username\", \"$password\")";
+	
+	if(mysqli_query($conn, $query)) {
+		echo '0';
+	} else {
+		echo "MySQL Error: " . mysqli_error($conn);
+	}
+}
+
+mysqli_close($conn);
+
+?>
